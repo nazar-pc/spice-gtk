19af039e421c27800b7bbea89cd721787969ed7b
Qiang Yu <yuq825@gmail.com>
2025-03-24 14:58:53 +0800

    spice-channel: read multiple fd from socket at once
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/spice-channel.c
+++ src/spice-channel.c
@@ -43,6 +43,7 @@
 #include <arpa/inet.h>
 #endif
 #include <ctype.h>
+#include <assert.h>
 
 #include "gio-coroutine.h"
 
@@ -928,17 +929,19 @@ static void spice_channel_write_msg(SpiceChannel *channel, SpiceMsgOut *out)
 }
 
 #ifdef G_OS_UNIX
-static ssize_t read_fd(int fd, int *msgfd)
+static ssize_t read_fds(int fd, int *msgfds, int max_num_fds)
 {
     struct msghdr msg = { NULL, };
     struct iovec iov[1];
     union {
         struct cmsghdr cmsg;
-        char control[CMSG_SPACE(sizeof(int))];
+        char control[CMSG_SPACE(sizeof(int) * 4)];
     } msg_control;
     struct cmsghdr *cmsg;
     ssize_t ret;
     char c;
+
+    assert(max_num_fds <= 4);
 
     iov[0].iov_base = &c;
     iov[0].iov_len = 1;
@@ -946,40 +949,39 @@ static ssize_t read_fd(int fd, int *msgfd)
     msg.msg_iov = iov;
     msg.msg_iovlen = 1;
     msg.msg_control = &msg_control;
-    msg.msg_controllen = sizeof(msg_control);
+    msg.msg_controllen = CMSG_SPACE(sizeof(int) * max_num_fds);
 
     ret = recvmsg(fd, &msg, 0);
     if (ret > 0) {
         for (cmsg = CMSG_FIRSTHDR(&msg);
              cmsg != NULL;
              cmsg = CMSG_NXTHDR(&msg, cmsg)) {
-            if (cmsg->cmsg_len != CMSG_LEN(sizeof(int)) ||
-                cmsg->cmsg_level != SOL_SOCKET ||
+            size_t cmsg_data_len;
+
+            if (cmsg->cmsg_level != SOL_SOCKET ||
                 cmsg->cmsg_type != SCM_RIGHTS) {
                 continue;
             }
 
-            memcpy(msgfd, CMSG_DATA(cmsg), sizeof(int));
-            if (*msgfd < 0) {
-                continue;
-            }
+            cmsg_data_len = cmsg->cmsg_len - CMSG_LEN(0);
+            memcpy(msgfds, CMSG_DATA(cmsg), cmsg_data_len);
+            break;
         }
     }
     return ret;
 }
 
 G_GNUC_INTERNAL
-gint spice_channel_unix_read_fd(SpiceChannel *channel)
+void spice_channel_unix_read_fds(SpiceChannel *channel, int *fds, int max_num_fds)
 {
     SpiceChannelPrivate *c = channel->priv;
-    gint fd = -1;
 
-    g_return_val_if_fail(g_socket_get_family(c->sock) == G_SOCKET_FAMILY_UNIX, -1);
+    g_return_if_fail(g_socket_get_family(c->sock) == G_SOCKET_FAMILY_UNIX);
 
     while (1) {
         /* g_socket_receive_message() is not convenient here because it
          * reads all control messages, and overly complicated to deal with */
-        if (read_fd(g_socket_get_fd(c->sock), &fd) > 0) {
+        if (read_fds(g_socket_get_fd(c->sock), fds, max_num_fds) > 0) {
             break;
         }
 
@@ -987,11 +989,9 @@ gint spice_channel_unix_read_fd(SpiceChannel *channel)
             g_coroutine_socket_wait(&c->coroutine, c->sock, G_IO_IN);
         } else {
             g_warning("failed to get fd: %s", g_strerror(errno));
-            return -1;
+            return;
         }
     }
-
-    return fd;
 }
 #endif
 
